5f35d928bb3f3eeafec5d6bdc17937d0b92fb407
Matt Hammond <mjhammond94@gmail.com>
2023-04-13 17:58:22 +0100

    Code debugged

--- lib/challenge_1.rb
+++ lib/challenge_1.rb
@@ -1,9 +1,11 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
-  text.chars.each do |char|
+  text.gsub(/\W/, "").chars.each do |char|
     counter[char] += 1
   end
-  p counter.to_a.sort_by { |k, v| v }[0][0]
+  counter.sort_by { |k, v| v }[-1][0]
 end
 
-get_most_common_letter("big ol bigs and his ol old biggens")
+get_most_common_letter("the roof, the roof, the roof is on fire!")
+# get_most_common_letter("the paddling pool, the paddling pool, the paddler is on fire!")
+# get_most_common_letter("A tester string")
